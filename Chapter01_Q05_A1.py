586a3a2fb95fc36a8fe3f409efc51bb16166ac15
saitota <1152469+saitota@users.noreply.github.com>
2018-04-01 16:03:30 +0900

    1st commit

--- Chapter01_Q05_A1.py
+++ Chapter01_Q05_A1.py
@@ -1,9 +1,10 @@
+import re
 odai_str = ('I am an NLPer')
 
-def ngram(n,target):
+def ngram(n,words):
     ansr_lst = []
-    for index in range(0, len(target) - n + 1):
-        ansr_lst.append(target[index:index + n])
+    for index in range(0, len(words) - n + 1):
+        ansr_lst.append(words[index:index + n])
     return ansr_lst
 
 print(ngram(2,re.split(' ', odai_str))) # 配列で渡すと単語n-gram
