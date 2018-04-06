1417f1c860faf3ba631d978ebb17174700ae5d30
saitota <1152469+saitota@users.noreply.github.com>
2018-04-06 11:15:35 +0900

    1st commit

---
+++ Chapter01_Q06_A3.py
@@ -0,0 +1,16 @@
+odai_str = ('paraparaparadise','paragraph')
+
+def ngram(n,words):
+    ansr_lst = []
+    for index in range(0, len(words) - n + 1):
+        ansr_lst.append(words[index:index + n])
+    return ansr_lst
+
+lst_x = ngram(2,odai_str[0])
+lst_y = ngram(2,odai_str[1])
+set_x = set(lst_x)
+set_y = set(lst_y)
+
+print(set_x | set_y)
+print(set_x & set_y)
+print(set_x - set_y)
