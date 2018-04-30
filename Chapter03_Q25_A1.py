fc03df4ed6347fc4f335caa04425db937b21c6bb
saitota <1152469+saitota@users.noreply.github.com>
2018-04-30 14:38:00 +0900

    added print

--- Chapter03_Q25_A1.py
+++ Chapter03_Q25_A1.py
@@ -3,5 +3,6 @@ import re
 match = re.findall('^\{\{基礎情報(.*?)\}\}$',dct_country.get('text'),re.MULTILINE+re.DOTALL)
 match2 = re.findall('\|(.*?) = (.*?)\n',match[0])
 match_dct = dict(match2)
+print (match_dct)
 # {'略名': 'イギリス', '日本語国名': 'グレートブリテン及び北アイルランド連合王国', '公式国名': '{{lang|en|United Kingdom of Great Britain and Northern Ireland}}<ref>英語以外での正式国名:<br/>', '国旗画像': 'Flag of the United Kingdom.svg', '国章画像': '[[ファイル:Royal Coat of Arms of the United Kingdom.svg|85px|イギリスの国章]]', '国章リンク': '（
 # (以下略)
